c02915a52215e5b03358fcb7a9ceb57ed9f13fd4
Igor Silveira <64613571+igorwsilveira@users.noreply.github.com>
2021-09-20 02:27:52 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -16,11 +16,11 @@ def hello_world():
     else:
         text = request.form['text']
         random_string = uuid.uuid4().hex
-        path = "static/" + random_string + ".svg"
-        model = load('model.joblib')
+        path = "app/static/" + random_string + ".svg"
+        model = load('app/model.joblib')
         np_arr = floats_string_to_np_arr(text)
-        make_picture('AgesAndHeights.pkl', model, np_arr, path) 
-        return render_template('index.html', href=path)
+        make_picture('app/AgesAndHeights.pkl', model, np_arr, path) 
+        return render_template('index.html', href=path[4:])
 
 def make_picture(training_data_filename, model, new_inp_np_arr, output_file):
   data = pd.read_pickle(training_data_filename)
